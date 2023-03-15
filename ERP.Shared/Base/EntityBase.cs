94d74074259c37ed4599c2b78aae5ef85f0492e4
amr rizk <amr.rizk@inthekloud.com>
2023-03-15 20:34:57 +0200

    init shared project

---
+++ ERP.Shared/Base/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP.Shared.Base
+{
+    public class EntityBase
+    {
+        public int Id { get; set; }
+        public string CreatedById { get; set; }
+        public string ModifiedById { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ ERP.Shared/Orders/OrderDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP.Shared.Orders
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string OrderStatus { get; set; }
+        public List<ProductDto> Products { get; set; }
+
+    }
+
+}
