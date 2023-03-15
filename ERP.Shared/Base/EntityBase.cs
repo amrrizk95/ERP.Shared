ffd73158500fdb401ebe8ad7c0e51be4bbbef302
amr rizk <amr.rizk@inthekloud.com>
2023-03-15 20:58:50 +0200

    change shared entity

--- ERP.Shared/Base/EntityBase.cs
+++ ERP.Shared/Base/EntityBase.cs
@@ -9,6 +9,8 @@ namespace ERP.Shared.Base
         public int Id { get; set; }
         public string CreatedById { get; set; }
         public string ModifiedById { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
         public bool IsDeleted { get; set; }
     }
 }
