a6da93bd88881bbb2267c323073cf1671236ee22
amr rizk <amr.rizk@inthekloud.com>
2023-03-16 20:28:44 +0200

    add order interface

---
+++ ERP.Shared/Orders/Interfaces/ICreateOrderMsg.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP.Shared.Orders.Interfaces
+{
+    public interface ICreateOrderMsg
+    {
+        public int Id { get; set; }
+        public ProductDto Product { get; set; }
+        public string OrderType { get; set; }
+    }
+}
